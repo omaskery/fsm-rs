e9ed912a42a6adfefc5b76a487b320a51f574e59
Oliver Maskery <omaskery@googlemail.com>
2015-08-24 08:07:38 +0100

    added documentation strings, but no examples yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use std::sync::Arc;
 
+/// Actions are just boxed immutable functions that take an argument of the event that triggered them
 pub type Action<E> = Box<Fn(E)>;
+/// StoredActions just enable structures holding the Action to be cloned
 type StoredAction<E> = Arc<Action<E>>;
 
+/// The Transition records, for a given current state, what event type triggers it to move to
+/// what state, performing a specific action on the transition
 #[derive(Clone)]
 struct Transition<S: Into<u32> + Clone, E: Into<u32> + Clone> {
 	event_type: E,
@@ -10,12 +14,15 @@ struct Transition<S: Into<u32> + Clone, E: Into<u32> + Clone> {
 	action: StoredAction<E>,
 }
 
+/// The StateTransition records all Transitions for a given state
 #[derive(Clone)]
 struct StateTransitions<S: Into<u32> + Clone, E: Into<u32> + Clone> {
 	start_state: S,
 	edges: Vec<Option<Transition<S, E>>>,
 }
 
+/// The Machine is the Finite State Machine, which has a current state and set of all valid
+/// transitions
 #[derive(Clone)]
 pub struct Machine<S: Into<u32> + Clone, E: Into<u32> + Clone> {
 	state: S,
@@ -23,6 +30,7 @@ pub struct Machine<S: Into<u32> + Clone, E: Into<u32> + Clone> {
 }
 
 impl<S: Into<u32> + Clone, E: Into<u32> + Clone> Machine<S, E> {
+	/// Constructs a new FSM with a given initial state
 	pub fn new(initial_state: S) -> Machine<S, E> {
 		Machine {
 			state: initial_state,
@@ -30,6 +38,7 @@ impl<S: Into<u32> + Clone, E: Into<u32> + Clone> Machine<S, E> {
 		}
 	}
 
+	/// Registers a new valid transition with the FSM
 	pub fn add_transition(&mut self, in_state: S, on_event: E, next_state: S, action: Action<E>) -> bool {
 		let state_index: u32 = in_state.clone().into();
 		let event_index: u32 = on_event.clone().into();
@@ -82,10 +91,12 @@ impl<S: Into<u32> + Clone, E: Into<u32> + Clone> Machine<S, E> {
 		result
 	}
 
+	/// Retrieves a reference to the current state
 	pub fn current_state(&self) -> &S {
 		&self.state
 	}
 
+	/// Tick the State Machine with an Event
 	pub fn on_event(&mut self, event_type: E) {
 		let event_index: u32 = event_type.clone().into();
 		let state_index: u32 = self.state.clone().into();
