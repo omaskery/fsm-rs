b41af07669d91237f082a4512fe383fc2c4d0982
Oliver Maskery <omaskery@googlemail.com>
2015-08-26 20:34:34 +0100

    tidied up implementation of code and added infrastructure for predicates on transitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,79 @@
-use std::sync::Arc;
 
 /// Actions are just boxed immutable functions that take an argument of the event that triggered them
-pub type Action<E> = Box<Fn(E)>;
-/// StoredActions just enable structures holding the Action to be cloned
-type StoredAction<E> = Arc<Action<E>>;
+pub type Action<S, E> = Box<Fn(&S,&E)>;
+
+/// Predicates are used to filter down whether a transition can occur
+pub type Predicate<S, E> = Box<Fn(&S,&E) -> bool>;
+
+/// Trait that should be trivially implementable for any Enum type
+pub trait EnumTag {
+	/// returns the discriminator tag for the enum (some_value as usize)
+	fn tag_number(&self) -> usize;
+	/// returns the highest discriminator tag for this enum
+	fn max_tag_number() -> usize;
+}
 
 /// The Transition records, for a given current state, what event type triggers it to move to
-/// what state, performing a specific action on the transition
-#[derive(Clone)]
-struct Transition<S: Into<u32> + Clone, E: Into<u32> + Clone> {
-	event_type: E,
+/// what state, performing a specific action on the transition, filterable by a predicate function
+struct Transition<S: EnumTag + Clone, E: EnumTag> {
+	predicate: Option<Predicate<S, E>>,
 	next_state: S,
-	action: StoredAction<E>,
+	action: Action<S, E>,
+}
+
+/// The EdgeTransitions record all the Transitions for a given event type
+struct EventTransitions<S: EnumTag + Clone, E: EnumTag> {
+	transitions: Vec<Transition<S, E>>,
 }
 
 /// The StateTransition records all Transitions for a given state
-#[derive(Clone)]
-struct StateTransitions<S: Into<u32> + Clone, E: Into<u32> + Clone> {
-	start_state: S,
-	edges: Vec<Option<Transition<S, E>>>,
+struct StateTransitions<S: EnumTag + Clone, E: EnumTag> {
+	edges: Vec<EventTransitions<S, E>>,
 }
 
 /// The Machine is the Finite State Machine, which has a current state and set of all valid
 /// transitions
-#[derive(Clone)]
-pub struct Machine<S: Into<u32> + Clone, E: Into<u32> + Clone> {
+pub struct Machine<S: EnumTag + Clone, E: EnumTag> {
 	state: S,
-	transitions: Vec<Option<StateTransitions<S, E>>>,
+	transitions: Vec<StateTransitions<S, E>>,
 }
 
-impl<S: Into<u32> + Clone, E: Into<u32> + Clone> Machine<S, E> {
+impl<S: EnumTag + Clone, E: EnumTag> Machine<S, E> {
 	/// Constructs a new FSM with a given initial state
 	pub fn new(initial_state: S) -> Machine<S, E> {
+		let mut transitions = Vec::with_capacity(S::max_tag_number());
+
+		for _ in 0..S::max_tag_number() + 1 {
+			let mut edges = Vec::with_capacity(E::max_tag_number());
+
+			for _ in 0..E::max_tag_number() + 1 {
+				edges.push(EventTransitions {
+					transitions: Vec::new(),
+				});
+			}
+
+			transitions.push(StateTransitions {
+				edges: edges,
+			});
+		}
+
 		Machine {
 			state: initial_state,
-			transitions: Vec::new(),
+			transitions: transitions,
 		}
 	}
 
 	/// Registers a new valid transition with the FSM
-	pub fn add_transition(&mut self, in_state: S, on_event: E, next_state: S, action: Action<E>) -> bool {
-		let state_index: u32 = in_state.clone().into();
-		let event_index: u32 = on_event.clone().into();
-		let mut result = false;
+	pub fn add_transition(&mut self, in_state: S, on_event: E, next_state: S, action: Action<S, E>) {
+		let transition = &mut self.transitions[in_state.tag_number()];
 
-		if state_index as usize >= self.transitions.capacity() {
-			let new_len = self.transitions.len() * 2;
-			self.transitions.reserve(new_len);
-		}
-		while state_index as usize >= self.transitions.len() {
-			self.transitions.push(None);
-		}
+		let edge = &mut transition.edges[on_event.tag_number()];
 
-		let create_state_entry = match self.transitions[state_index as usize] {
-			Some(_) => false,
-			None => true,
-		};
-
-		if create_state_entry {
-			self.transitions[state_index as usize] = Some(StateTransitions {
-				start_state: in_state,
-				edges: Vec::new(),
-			});
-		}
-
-		if let &mut Some(ref mut transitions) = &mut self.transitions[state_index as usize] {
-			if event_index as usize >= transitions.edges.len() {
-				let new_len = transitions.edges.len() * 2;
-				transitions.edges.reserve(new_len);
-			}
-			while event_index as usize >= transitions.edges.len() {
-				transitions.edges.push(None);
-			}
-
-			let transition = &mut transitions.edges[event_index as usize];
-
-			*transition = match &*transition {
-				&Some(ref t) => Some((*t).clone()),
-				&None => {
-					result = true;
-					Some(Transition {
-						event_type: on_event,
-						next_state: next_state,
-						action: Arc::new(action),
-					})
-				}
-			};
-		}
-
-		result
+		edge.transitions.push(Transition {
+			predicate: None,
+			action: action,
+			next_state: next_state,
+		});
 	}
 
 	/// Retrieves a reference to the current state
@@ -98,21 +83,19 @@ impl<S: Into<u32> + Clone, E: Into<u32> + Clone> Machine<S, E> {
 
 	/// Tick the State Machine with an Event
 	pub fn on_event(&mut self, event_type: E) {
-		let event_index: u32 = event_type.clone().into();
-		let state_index: u32 = self.state.clone().into();
-		
-		if state_index as usize >= self.transitions.len() {
-			return;
-		}
+		let transition = &self.transitions[self.state.tag_number()];
+		let edge = &transition.edges[event_type.tag_number()];
 
-		if let &Some(ref state_transitions) = &self.transitions[state_index as usize] {
-			if event_index as usize >= state_transitions.edges.len() {
-				return;
-			}
+		for transition in edge.transitions.iter() {
+			let valid = match &transition.predicate {
+				&Some(ref p) => (*p)(&self.state, &event_type),
+				&None => true,
+			};
 
-			if let &Some(ref transition) = &state_transitions.edges[event_index as usize] {
+			if valid {
+				(*transition.action)(&self.state, &event_type);
 				self.state = transition.next_state.clone();
-				(*(*transition).action)(event_type);
+				break;
 			}
 		}
 	}
@@ -128,21 +111,33 @@ mod test {
 		Unlocked,
 	}
 
-	#[derive(Clone, Debug, Eq, PartialEq)]
+	#[derive(Debug, Eq, PartialEq)]
 	enum TurnStyleEvent {
 		Push,
 		InsertCoin,
 	}
 
-	impl Into<u32> for TurnStyleState {
-		fn into(self) -> u32 {
-			self as u32
+	impl EnumTag for TurnStyleState {
+		fn tag_number(&self) -> usize {
+			match self {
+				&TurnStyleState::Locked => 0,
+				&TurnStyleState::Unlocked => 1
+			}
+		}
+		fn max_tag_number() -> usize {
+			TurnStyleState::Unlocked as usize
 		}
 	}
 
-	impl Into<u32> for TurnStyleEvent {
-		fn into(self) -> u32 {
-			self as u32
+	impl EnumTag for TurnStyleEvent {
+		fn tag_number(&self) -> usize {
+			match self {
+				&TurnStyleEvent::Push => 0,
+				&TurnStyleEvent::InsertCoin => 1,
+			}
+		}
+		fn max_tag_number() -> usize {
+			TurnStyleEvent::InsertCoin as usize
 		}
 	}
 
@@ -151,11 +146,11 @@ mod test {
 		let mut machine = Machine::new(TurnStyleState::Locked);
 		machine.add_transition(
 			TurnStyleState::Locked, TurnStyleEvent::InsertCoin,
-			TurnStyleState::Unlocked, Box::new(|_| println!("unlocked"))
+			TurnStyleState::Unlocked, Box::new(|_,_| println!("unlocked"))
 		);
 		machine.add_transition(
 			TurnStyleState::Unlocked, TurnStyleEvent::Push,
-			TurnStyleState::Locked, Box::new(|_| println!("locked"))
+			TurnStyleState::Locked, Box::new(|_,_| println!("locked"))
 		);
 		assert!(*machine.current_state() == TurnStyleState::Locked);
 		machine.on_event(TurnStyleEvent::Push);
