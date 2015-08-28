13699c6a99d95b91750e6c7271151fb447869f89
Oliver Maskery <omaskery@googlemail.com>
2015-08-28 16:22:35 +0100

    removed clone dependancy, added need to construct enum from usize, no longer advise storing variables in state structure

--- src/lib.rs
+++ src/lib.rs
@@ -11,34 +11,36 @@ pub trait EnumTag {
 	fn tag_number(&self) -> usize;
 	/// returns the highest discriminator tag for this enum
 	fn max_tag_number() -> usize;
+	/// returns an instance of the enum from a tag number
+	fn from_tag(tag: usize) -> Self;
 }
 
 /// The Transition records, for a given current state, what event type triggers it to move to
 /// what state, performing a specific action on the transition, filterable by a predicate function
-struct Transition<S: EnumTag + Clone, E: EnumTag> {
+struct Transition<S: EnumTag, E: EnumTag> {
 	predicate: Option<Predicate<S, E>>,
 	next_state: S,
 	action: Action<S, E>,
 }
 
 /// The EdgeTransitions record all the Transitions for a given event type
-struct EventTransitions<S: EnumTag + Clone, E: EnumTag> {
+struct EventTransitions<S: EnumTag, E: EnumTag> {
 	transitions: Vec<Transition<S, E>>,
 }
 
 /// The StateTransition records all Transitions for a given state
-struct StateTransitions<S: EnumTag + Clone, E: EnumTag> {
+struct StateTransitions<S: EnumTag, E: EnumTag> {
 	edges: Vec<EventTransitions<S, E>>,
 }
 
 /// The Machine is the Finite State Machine, which has a current state and set of all valid
 /// transitions
-pub struct Machine<S: EnumTag + Clone, E: EnumTag> {
-	state: S,
+pub struct Machine<S: EnumTag, E: EnumTag> {
+	state: usize,
 	transitions: Vec<StateTransitions<S, E>>,
 }
 
-impl<S: EnumTag + Clone, E: EnumTag> Machine<S, E> {
+impl<S: EnumTag, E: EnumTag> Machine<S, E> {
 	/// Constructs a new FSM with a given initial state
 	pub fn new(initial_state: S) -> Machine<S, E> {
 		let mut transitions = Vec::with_capacity(S::max_tag_number());
@@ -58,7 +60,7 @@ impl<S: EnumTag + Clone, E: EnumTag> Machine<S, E> {
 		}
 
 		Machine {
-			state: initial_state,
+			state: initial_state.tag_number(),
 			transitions: transitions,
 		}
 	}
@@ -77,24 +79,24 @@ impl<S: EnumTag + Clone, E: EnumTag> Machine<S, E> {
 	}
 
 	/// Retrieves a reference to the current state
-	pub fn current_state(&self) -> &S {
-		&self.state
+	pub fn current_state(&self) -> S {
+		S::from_tag(self.state)
 	}
 
 	/// Tick the State Machine with an Event
 	pub fn on_event(&mut self, event_type: E) {
-		let transition = &self.transitions[self.state.tag_number()];
+		let transition = &self.transitions[self.state];
 		let edge = &transition.edges[event_type.tag_number()];
 
 		for transition in edge.transitions.iter() {
 			let valid = match &transition.predicate {
-				&Some(ref p) => (*p)(&self.state, &event_type),
+				&Some(ref p) => (*p)(&self.current_state(), &event_type),
 				&None => true,
 			};
 
 			if valid {
-				(*transition.action)(&self.state, &event_type);
-				self.state = transition.next_state.clone();
+				(*transition.action)(&self.current_state(), &event_type);
+				self.state = transition.next_state.tag_number();
 				break;
 			}
 		}
@@ -105,7 +107,7 @@ impl<S: EnumTag + Clone, E: EnumTag> Machine<S, E> {
 mod test {
 	use super::*;
 
-	#[derive(Clone, Debug, Eq, PartialEq)]
+	#[derive(Debug, Eq, PartialEq)]
 	enum TurnStyleState {
 		Locked,
 		Unlocked,
@@ -127,17 +129,31 @@ mod test {
 		fn max_tag_number() -> usize {
 			TurnStyleState::Unlocked as usize
 		}
+		fn from_tag(tag: usize) -> TurnStyleState {
+			match tag {
+				0 => TurnStyleState::Locked,
+				1 => TurnStyleState::Unlocked,
+				_ => panic!("invalid tag number")
+			}
+		}
 	}
 
 	impl EnumTag for TurnStyleEvent {
 		fn tag_number(&self) -> usize {
 			match self {
 				&TurnStyleEvent::Push => 0,
-				&TurnStyleEvent::InsertCoin => 1,
+				&TurnStyleEvent::InsertCoin => 1
 			}
 		}
 		fn max_tag_number() -> usize {
 			TurnStyleEvent::InsertCoin as usize
+		}
+		fn from_tag(tag: usize) -> TurnStyleEvent {
+			match tag {
+				0 => TurnStyleEvent::Push,
+				1 => TurnStyleEvent::InsertCoin,
+				_ => panic!("invalid tag number")
+			}
 		}
 	}
 
@@ -152,14 +168,14 @@ mod test {
 			TurnStyleState::Unlocked, TurnStyleEvent::Push,
 			TurnStyleState::Locked, Box::new(|_,_| println!("locked"))
 		);
-		assert!(*machine.current_state() == TurnStyleState::Locked);
+		assert!(machine.current_state() == TurnStyleState::Locked);
 		machine.on_event(TurnStyleEvent::Push);
-		assert!(*machine.current_state() == TurnStyleState::Locked);
+		assert!(machine.current_state() == TurnStyleState::Locked);
 		machine.on_event(TurnStyleEvent::InsertCoin);
-		assert!(*machine.current_state() == TurnStyleState::Unlocked);
+		assert!(machine.current_state() == TurnStyleState::Unlocked);
 		machine.on_event(TurnStyleEvent::InsertCoin);
-		assert!(*machine.current_state() == TurnStyleState::Unlocked);
+		assert!(machine.current_state() == TurnStyleState::Unlocked);
 		machine.on_event(TurnStyleEvent::Push);
-		assert!(*machine.current_state() == TurnStyleState::Locked);
+		assert!(machine.current_state() == TurnStyleState::Locked);
 	}
 }
